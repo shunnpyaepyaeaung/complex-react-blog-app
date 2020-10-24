7b1d67dfb48abfe27a1f57c037fe9196961bbd70
Shunn Pyae Pyae Aung <shunnpyaepyaeaung@Shunns-MacBook-Pro.local>
2020-10-24 21:54:44 +0630

    routing-single-page-application

--- app/Main.js
+++ app/Main.js
@@ -3,17 +3,30 @@ import ReactDOM from 'react-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeGuest from './components/HomeGuest';
+import { BrowserRouter,Switch, Route} from 'react-router-dom';
+import About from './components/About';
+import Terms from './components/Terms';
 
 function Main() {
     return (
-        <>
+        <BrowserRouter>
             <div>
                 <Header/>
-                <HomeGuest/>
+                <Switch>
+                    <Route path="/" exact>
+                        <HomeGuest/>
+                    </Route>
+                    <Route path="/about-us">
+                        <About/>
+                    </Route>
+                    <Route path="/terms">
+                        <Terms/>
+                    </Route>
+                </Switch>
                 <Footer/> 
             </div>
 
-        </>
+        </BrowserRouter>
     )
 }
 
